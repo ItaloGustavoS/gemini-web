4fb06e6e26ae3ed8e2bad026e798524a3ee2f793
ItaloGustavoS <42496107+ItaloGustavoS@users.noreply.github.com>
2024-05-06 17:48:26 -0300

    "Added state for error handling and input field for user input in search section"
    The changes include:
    
    * Importing `useState` hook from React.
    * Adding a state variable `Error` and its corresponding setter

--- gemini-web/src/App.js
+++ gemini-web/src/App.js
@@ -1,9 +1,17 @@
+import { useState } from "react";
+
 const App = () => {
+  const [Error, setError] = useState("");
   return (
     <div className="app">
       <section className="search-section">
         <p>Olá, como posso te ajudar ?</p>
-        <button className="surprise">Enviar</button>
+        <button className="surprise">Me Surpreenda!</button>
+        <div className="input-container">
+          <input value={""} placeholder="Quando e o Natal...?" onChange={""} />
+          {!Error && <button>Enviar</button>}
+          {Error && <button>Apagar</button>}
+        </div>
       </section>
     </div>
   );
