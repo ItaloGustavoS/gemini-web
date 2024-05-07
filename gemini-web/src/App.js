f88acfc9ad1ec60c082e0dd034fba2fda44075b1
ItaloGustavoS <42496107+ItaloGustavoS@users.noreply.github.com>
2024-05-07 10:45:16 -0300

    "Add surprise feature and input value state; update getResponse function to include chat history"
    This commit adds a surprise feature that generates a random question when the "Me Surpreenda!" button is clicked. It also adds a state for the

--- gemini-web/src/App.js
+++ gemini-web/src/App.js
@@ -1,13 +1,56 @@
 import { useState } from "react";
 
 const App = () => {
+  const [value, setValue] = useState("");
   const [Error, setError] = useState("");
+  const [chatHistory, setChatHistory] = useState([]);
+
+  const surpriseOptions = [
+    "Quem ganhou o ultimo Premio Nobel ?",
+    "Qual a maior montanha do mundo ?",
+    "Qual a maior floresta do mundo ?",
+    "Qual o maior rio do mundo ?",
+    "Qual a maior cidade do mundo ?",
+  ];
+  const surprise = () => {
+    const randomValue =
+      surpriseOptions[Math.floor(Math.random() * surpriseOptions.length)];
+    setValue(randomValue);
+  };
+
+  const getResponse = async () => {
+    if (!value) {
+      setError("Erro! Por favor faça uma pergunta!");
+      return;
+    }
+    try {
+      const options = {
+        method: "POST",
+        body: JSON.stringify({
+          history: chatHistory,
+          message: value,
+        }),
+      };
+    } catch (error) {
+      console.error(error);
+      setError(
+        "Alguma coisa deu errado! Por favor, tente novamente mais tarde."
+      );
+    }
+  };
+
   return (
     <section className="app">
       <p>Olá, como posso te ajudar ?</p>
-      <button className="surprise">Me Surpreenda!</button>
+      <button className="surprise" onClick={surprise} disabled={!chatHistory}>
+        Me Surpreenda!
+      </button>
       <div className="input-container">
-        <input value={""} placeholder="Quando e o Natal...?" onChange={""} />
+        <input
+          value={value}
+          placeholder="Quando e o Natal...?"
+          onChange={(e) => setValue(e.target.value)}
+        />
         {!Error && <button>Enviar</button>}
         {Error && <button>Apagar</button>}
       </div>
