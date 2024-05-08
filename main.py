2017d9223f7e26a9e7b6540fefe4d2992d0917c6
ItaloGustavoS <42496107+ItaloGustavoS@users.noreply.github.com>
2024-05-08 18:09:15 -0300

    V0.1 - Testing PDF Upload *Broken* "Updated model configuration with safety settings and added function to extract text from PDFs"
    This commit updates the configuration of the generative model to include safety settings and a threshold for blocking certain types of harmful content. Additionally, a new function `

--- main.py
+++ main.py
@@ -1,13 +1,50 @@
 import os
 import streamlit as st
 import google.generativeai as genai
+from pypdf import PdfReader
 
 genai.configure(api_key="AIzaSyCEk7rv5K2DnnfLwOoQx0uDgU2LTpmycBQ")
-model = genai.GenerativeModel("models/gemini-1.5-pro-latest")
+
+generation_config = {
+    "temperature": 1,
+    "top_p": 0.95,
+    "top_k": 0,
+    "max_output_tokens": 8192,
+}
+
+safety_settings = [
+    {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+    {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+    },
+]
+
+model = genai.GenerativeModel(
+    model_name="models/gemini-1.5-pro-latest",
+    generation_config=generation_config,
+    safety_settings=safety_settings,
+)
 
 st.set_page_config(page_title="Gemini AI", page_icon=":computer:")
 
 st.title("Gemini AI :computer:")
+
+
+def extract_pdf_pages(pathname: str) -> list[str]:
+    parts = [f"--- START OF PDF ${pathname} ---"]
+    # Add logic to read the PDF and return a list of pages here.
+    pages = []
+    for index, page in enumerate(pages):
+        parts.append(f"--- PAGE {index} ---")
+        parts.append(page)
+    return parts
+
 
 if "chat" not in st.session_state:
     st.session_state.chat = model.start_chat(history=[])
