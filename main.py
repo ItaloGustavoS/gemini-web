786440aa733c6859a01e92d4c7e77904955db8c8
ItaloGustavoS <42496107+ItaloGustavoS@users.noreply.github.com>
2024-05-16 14:16:08 -0300

    "Updated generation config, added safety settings and system instruction, and modified model initialization"

--- main.py
+++ main.py
@@ -11,11 +11,28 @@ GOOGLE_API_KEY = "AIzaSyCeenyy-kc_VW9ljErzH2OecYx9NtKt-2U"
 genai.configure(api_key=GOOGLE_API_KEY)
 
 # Configuração para geração de texto
-generation_config = {"temperature": 0.5, "candidate_count": 1}
+generation_config = {
+    "temperature": 0,
+    "top_p": 0.95,
+    "top_k": 64,
+    "max_output_tokens": 8192,
+}
+
+safety_settings = [
+    {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_LOW_AND_ABOVE"},
+    {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_LOW_AND_ABOVE"},
+    {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_LOW_AND_ABOVE"},
+    {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_LOW_AND_ABOVE"},
+]
+
+system_instruction = "Quando o usuário fizer a pergunta, leia o arquivo pdf fornecido e exiba as informações dele na resposta"
 
 # Gera o texto formatado
 model1 = genai.GenerativeModel(
-    "models/gemini-1.5-pro-latest", generation_config=generation_config
+    model_name="gemini-1.5-flash-latest",
+    generation_config=generation_config,
+    system_instruction=system_instruction,
+    safety_settings=safety_settings,
 )
 
 # Configuração do Streamlit
